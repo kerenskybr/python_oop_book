50e568df756e2d481c293df23ccf109391cd7ae1
keresnkybr <roger.o.monteiro@gmail.com>
2021-03-01 17:02:00 -0300

    Add more study cases

--- case_study_rental/rent.py
+++ case_study_rental/rent.py
@@ -23,7 +23,7 @@ class Property:
 
 
 class Apartment(Property):
-	valid_laudries = ("coin", "ensuite", "none")
+	valid_laundries = ("coin", "ensuite", "none")
 	valid_balconies = ("yes", "no", "solarium")
 
 	def __init__(self, balcony='', laundry='', **kwargs):
@@ -64,3 +64,141 @@ def get_valid_input(input_string, valid_options):
 		response = input(input_string)
 	
 	return response
+
+
+class House(Property):
+	valid_garage = ("attached", "detached", "none")
+	valid_fenced = ("yes", "no")
+
+	def __init__(self, num_stories="", garage="", fenced="", **kwargs):
+		super().__init__(**kwargs)
+		self.garage = garage
+		self.fenced = fenced
+		self.num_stories = num_stories
+
+	def display(self):
+		super().display()
+		print("HOUSE DETAILS")
+		print("# of stories: ({})".format(self.num_stories))
+		print("garage: {}".format(self.garage))
+		print("fenced yard: {}".format(self.fenced))
+
+	def prompt_init():
+		parent_init = Property.prompt_init()
+		fenced = get_valid_input("Is the yard fenced? ",
+			House.valid_fenced)
+		garage = get_valid_input("Is there a garage? ",
+			House.valid_garage)
+
+		num_stories = input("How many stories? ")
+		parent_init.update({
+			"fenced": fenced,
+			"garage": garage,
+			"num_stories": num_stories
+			})
+		return parent_init
+
+	prompt_init = staticmethod(prompt_init)
+
+
+class Purchase:
+	def __init__(self, price="", taxes="", **kwargs):
+		super().__init__(**kwargs)
+		self.price = price
+		self.taxes = taxes
+
+	def display(self):
+		super().display()
+		print("PURCHASE DETAILS")
+		print("selling price: {}".format(self.price))
+		print("estimated taxes: {}".format(self.taxes))
+
+	def prompt_init():
+		return dict(
+			price = input("what's the selling price? "),
+			taxes = input("What are the estimated taxes? ")
+			)
+
+	prompt_init = staticmethod(prompt_init)
+
+
+class Rental:
+	def __init__(self, furnished="", utilities="", rent="", **kwargs):
+		super().__init__(**kwargs)
+		self.furnished = furnished
+		self.rent = rent
+		self.utilities = utilities
+
+	def display(self):
+		super().display()
+		print("RENTAL DETAILS")
+		print("rent: {}".format(self.rent))
+		print("estimated utilities: {}".format(self.utilities))
+		print("furnished: {}".format(self.furnished))
+	
+	def prompt_init():
+		return dict(rent=input("What is the monthly rent? "), utilities=input("What are the estimated utilities? "),furnished=get_valid_input("Is the property furnished? ",("yes", "no")))
+	
+	prompt_init = staticmethod(prompt_init)
+
+# Nao é  necessario chamar init ou super aqui
+# pois ambas classes herdadas chamam super e nao sabemos
+# a ordem que elas irao tomar
+class HouseRental(Rental, House):
+	def prompt_init():
+		init = House.prompt_init()
+		init.update(Rental.prompt_init())
+		return init
+
+	prompt_init = staticmethod(prompt_init)
+
+class ApartmentRental(Rental, Apartment):
+	def prompt_init():
+		init = Apartment.prompt_init()
+		init.update(Rental.prompt_init())
+		return init
+	
+	prompt_init = staticmethod(prompt_init)
+
+class ApartmentPurchase(Purchase, Apartment):
+	def prompt_init():
+		init = Apartment.prompt_init()
+		init.update(Purchase.prompt_init())
+		return init
+	
+	prompt_init = staticmethod(prompt_init)
+
+class HousePurchase(Purchase, House):
+	def prompt_init():
+		init = House.prompt_init()
+		init.update(Purchase.prompt_init())
+		return init
+
+	prompt_init = staticmethod(prompt_init)
+
+class Agent:
+	def __init__(self):
+		self.property_list = []
+	
+	def display_properties(self):
+		for property in self.property_list:
+			property.display()
+
+	type_map = {
+		("house", "rental"): HouseRental,
+		("house", "purchase"): HousePurchase,
+		("apartment", "rental"): ApartmentRental,
+		("apartment", "purchase"): ApartmentPurchase
+		}
+
+	def add_property(self):
+		property_type = get_valid_input(
+			"What type of property? ",
+			("house", "apartment")).lower()
+		payment_type = get_valid_input(
+			"What payment type? ",
+			("purchase", "rental")).lower()
+		PropertyClass = self.type_map[
+			(property_type, payment_type)]
+		init_args = PropertyClass.prompt_init()
+		self.property_list.append(PropertyClass(**init_args))
